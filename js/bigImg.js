d39915e88dd0894aa7a38893cc33d361af9f6455
Sophie-Nie <xinyunie90@gmail.com>
2025-03-12 02:05:54 +0000

    Update

---
+++ js/bigImg.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.ner img, #starsIF img').forEach(img => {
+      img.onclick = function () {
+        showModal(this.src);
+      }
+    });
+  
+    function showModal(src) {
+      const modal = document.getElementById('modal');
+      const modalImg = document.getElementById('modal-image');
+      modal.style.display = 'flex';
+      modalImg.src = src;
+    }
+  
+    function hideModal() {
+      document.getElementById('modal').style.display = 'none';
+    }
+  
+    document.getElementById('modal').onclick = function (e) {
+      if (e.target === this) hideModal();
+    }
+  });
